0e87de58254135975d32ecaa7f46e820e3359aca
xujiucheng <xjc851191369@163.com>
2021-05-25 17:47:17 +0800

    2021.05.25 17:48代码更新

--- src/com/xjc/study/test/Test.java
+++ src/com/xjc/study/test/Test.java
@@ -1,9 +1,27 @@
 package com.xjc.study.test;
 
-import java.math.BigDecimal;
+import com.xjc.study.toolutils.IntegerUtils;
 
+/**
+ * @description:
+ * @author: xujiucheng
+ * @date: 2021/5/25 10:05 
+ */
 public class Test {
+
     public static void main(String[] args) {
-        System.out.println(new BigDecimal("25000").divide(new BigDecimal("2")));
+        System.out.println(IntegerUtils.nextDouble(1000.00,5000.00));
+    }
+
+    /**
+     * @description:
+     * @author: xujiucheng
+     * @date: 2021/5/25 14:16 
+     * @param:  * @param null
+     * @exception:
+     * @return: {@link null} 
+     */
+    public String test(String str) {
+        return null;
     }
 }
--- src/com/xjc/study/toolutils/IntegerUtils.java
+++ src/com/xjc/study/toolutils/IntegerUtils.java
@@ -1,5 +1,9 @@
 package com.xjc.study.toolutils;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Random;
+
 /**
  * Description: TODO
  *
@@ -15,5 +19,25 @@ public class IntegerUtils {
      **/
     public static int generatorInt(int minValue, int maxValue) {
         return (int)(Math.random() + minValue) + (int)((maxValue - minValue) * Math.random());
+    }
+
+    /**
+     * @escription: 随机生成浮点数
+     * @Author: XuJiuCheng
+     * @Date: 2021/5/25 17:42
+     * @Param:  * @param null
+     * @Exception:
+     * @Return: {@link null}
+     */
+    public static double nextDouble(final double min, final double max) {
+        //保留两位小数
+        DecimalFormat df = new DecimalFormat("#.00");
+        if (max < min) {
+            return min - max;
+        }
+        if (min == max) {
+            return min;
+        }
+        return  Double.parseDouble(df.format(min + ((max - min) * new Random().nextDouble())));
     }
 }
