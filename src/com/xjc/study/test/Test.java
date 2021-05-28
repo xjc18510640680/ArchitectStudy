17c90b62b2769e51929697f2124981c3b91968de
xujiucheng <xjc851191369@163.com>
2021-05-28 13:55:37 +0800

    2021.05.28 13:55代码更新

--- src/com/xjc/study/test/Test.java
+++ src/com/xjc/study/test/Test.java
@@ -9,8 +9,19 @@ import com.xjc.study.toolutils.IntegerUtils;
  */
 public class Test {
 
+    private static final double MAX = 10000.00;
+
     public static void main(String[] args) {
-        System.out.println(IntegerUtils.nextDouble(1000.00,5000.00));
+        System.out.println(0.65*40000);
+        double a = IntegerUtils.nextDouble(1000.00,5000.00);
+        double b = IntegerUtils.nextDouble(1000.00,5000.00 - a);
+        double c = IntegerUtils.nextDouble(1000.00,5000.00 - (a - b));
+        double d = IntegerUtils.nextDouble(1000.00,5000.00 - (a - b -c));
+        double e = IntegerUtils.nextDouble(1000.00,5000.00 - (a - b - c - d));
+        if (a + b + c + d + e <= MAX) {
+            System.out.println(a + b + c + d + e);
+            System.out.println(a + "===" + b + "===" + c + "===" + d + "===" + e);
+        }
     }
 
     /**
